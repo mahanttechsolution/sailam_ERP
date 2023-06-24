a6709b0d4b220bd8eb67f739e504eb6ceefa35c5
rfenil <frangani08@gmail.com>
2023-06-24 11:16:41 +0530

    Done: Message system

--- sailam_ERP/message/format.py
+++ sailam_ERP/message/format.py
@@ -2,6 +2,7 @@ from .models import Message
 
 formatstr='{0}: for {1} - {2} {3} is {4} by {5}- {6}'
 inventory='{0}: Diamond with StockId {1} is {2} by {3}'
+parcel='{0}: {0} With Id:{1} is {2} by {3} '
 
 
 def saveMessage(type,user,*args,**kwargs):
@@ -15,6 +16,14 @@ def saveMessage(type,user,*args,**kwargs):
 
     elif type=='inventory':
      mess=inventory.format(*args)
+     if 'changes' in kwargs:
+      changes=kwargs.get("changes")
+      Message.objects.create(Sender=user,Message=mess,Changes=changes)
+     else:
+      Message.objects.create(Sender=user,Message=mess)
+
+    elif type=='parcel':
+     mess=parcel.format(*args)
      if 'changes' in kwargs:
       changes=kwargs.get("changes")
       Message.objects.create(Sender=user,Message=mess,Changes=changes)
