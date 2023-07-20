89742d79ec7b2986124e00894032159d51a33445
TeerthSurani2311 <125901349+TeerthSurani2311@users.noreply.github.com>
2023-07-20 13:10:31 +0530

    Update views.py

--- sailam_ERP/account/views.py
+++ sailam_ERP/account/views.py
@@ -161,10 +161,10 @@ def register(request):
                 'bought_data': bought_data,
                 'sold_data': sold_data
             }
-            role = User.objects.get(email=user)
-            if str(role.role) == 'user':
+           role = User.objects.get(email=user)
+           if str(role.role) == 'user':
                 return redirect('stocks')
-            else:
+           else:
                 return redirect('dashboard')
     else:
      return render(request,'account/register.html')
